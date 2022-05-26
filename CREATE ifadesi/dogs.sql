ff83022ade990d7ae09ba4ecd2e468e12fb41340
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-26 21:21:19 +0300

    Update dogs.sql

--- CREATE ifadesi/dogs.sql
+++ CREATE ifadesi/dogs.sql
@@ -1,7 +1,6 @@
 /*
 Who let the dogs out
 
-4967 users solved this problem. Latest completion was less than a minute ago.
 Create a table named dogs with three columns: dog_id (INT), name (VARCHAR(20)), and age (INT).
 */
 
