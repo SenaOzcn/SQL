6c478dd1d2cf9a0570c9fbb114be9ca05222d0c1
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-26 20:53:55 +0300

    Create schools.sql

---
+++ CREATE ifadesi/schools.sql
@@ -0,0 +1,12 @@
+/*
+Back to school
+
+4708 users solved this problem. Latest completion was less than a minute ago.
+Create a table named schools with three columns: school_num(INT), city_id(INT), and foundation_date(DATE).
+*/
+
+CREATE TABLE schools(
+    school_num INT,
+    city_id INT,
+    foundation_date DATE
+);
