7ba381bceee285e363e07047664f3719f624f87a
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-26 20:43:28 +0300

    Create education.sql

---
+++ CREATE ifadesi/education.sql
@@ -0,0 +1,8 @@
+/*
+We don't need no Education
+ Easy
+6242 users solved this problem. Latest completion was less than a minute ago.
+Delete the table named education with columns name (VARCHAR(20)), school_number (INT) and university (VARCHAR(20)).
+*/
+
+DROP TABLE education;
