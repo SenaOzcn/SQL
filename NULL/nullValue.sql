d72570c2195cc560939cdd347de68a728fcd0f56
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-26 21:20:40 +0300

    Create nullValue.sql

---
+++ NULL/nullValue.sql
@@ -0,0 +1,8 @@
+/*
+Truth wins
+
+Latest completion was less than a minute ago.
+Write a query that evaluates an expression that contains TRUE, FALSE, and NULL, but still returns TRUE.
+*/
+
+SELECT TRUE OR FALSE OR NULL;
