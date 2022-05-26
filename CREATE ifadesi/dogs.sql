2dc9e21c5c976fb0b202b67c4550f788aef743d2
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-26 20:51:51 +0300

    Create dogs.sql

---
+++ CREATE ifadesi/dogs.sql
@@ -0,0 +1,12 @@
+/*
+Who let the dogs out
+
+4967 users solved this problem. Latest completion was less than a minute ago.
+Create a table named dogs with three columns: dog_id (INT), name (VARCHAR(20)), and age (INT).
+*/
+
+CREATE TABLE dogs(
+    dog_id INT,
+    name VARCHAR(20),
+    age INT
+);
