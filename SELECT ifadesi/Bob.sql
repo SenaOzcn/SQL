d04ab759a54c307bd8ddefa0876c53c60b33bd3a
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-27 12:37:58 +0300

    Create Bob.sql

---
+++ SELECT ifadesi/Bob.sql
@@ -0,0 +1,17 @@
+/*
+In a table, there is some information on a user named Bob:
+
+ID	name	username	karma	topic
+542	Bob	    B0b	     4.3	 IT
+Write the query that will select all information about Bob. Use column titles as aliases.
+
+You do not need to use the FROM keyword for this task. Do not forget to use quotes.
+*/
+
+SELECT
+542     AS  "ID",
+'Bob'   AS  "name",
+'B0b'   AS  "username",
+4.3     AS  "karma",
+'IT'    AS  "topic"
+;
