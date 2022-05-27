1804b8e99bf08ea319bce2b7c819fdb70d4fce76
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-27 12:47:02 +0300

    Create calculations.sql

---
+++ SELECT ifadesi/calculations.sql
@@ -0,0 +1,10 @@
+/*
+Write a query that evaluates 2+2, 2-2, 2*2, and 2/2. Do not change the order of values.
+*/
+
+SELECT
+ 2+2  AS "Addition", 
+ 2-2  AS "Subtraction", 
+ 2*2  AS "Multiplication",
+ 2/2  AS "Division"
+;
