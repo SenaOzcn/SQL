37875682cd2a4679c6fdb6dd0a4b9c91584e442c
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-06-05 15:50:15 +0300

    Create Census.sql

---
+++ SELECT FROM deyimi/Census.sql
@@ -0,0 +1,18 @@
+/*
+A table is defined by the following statement:
+
+CREATE TABLE Census (id INTEGER, 
+                     name VARCHAR(20), 
+                     birth_year INTEGER, 
+                     gender VARCHAR(1));
+Write a query that selects all the columns without the usage of a * symbol.
+*/
+
+SELECT
+    id         AS "id",
+    name       AS "name",
+    birth_year AS "birth year",
+    gender     AS "gender"
+FROM
+    Census
+;
